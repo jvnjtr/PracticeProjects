a9f78f66bd4e37c3279547e3eb006fcbf789e204
jivan <jvnjtr@gmail.com>
2026-05-08 19:01:19 +0530

    new angular project from udemy

--- Node/PRACTICE IN 2026/Node_mysql_jwt/app.js
+++ Node/PRACTICE IN 2026/Node_mysql_jwt/app.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2/promise");
+require('dotenv').config();
 
 // const mysql_db=await mysql.createConnection({
 //   host:'localhost',
@@ -12,10 +13,10 @@ const mysql = require("mysql2/promise");
 //await mysql_db.execute(`CREATE database node_practice`)
 
 const mysqlPool=mysql.createPool({
-  host:'localhost',
-  user:'root',
-  password:'',
-  database:'node_practice'
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME
 });
 
 
--- Node/PRACTICE IN 2026/Node_mysql_jwt/index.js
+++ Node/PRACTICE IN 2026/Node_mysql_jwt/index.js
@@ -6,7 +6,6 @@ const db=require('./app');
 db.query("SELECT 1")
   .then(data => {
     console.log(data);
-
     // Start server AFTER DB is connected
     app.listen(3000, () => {
       console.log('Server running at port 3000');
